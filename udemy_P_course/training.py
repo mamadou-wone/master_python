e122d5cce82b149005ac229440f8907ba1a64a1c
Mamadou WONE <mamadouwone12345@gmail.com>
2021-01-24 12:07:50 +0000

    continue day8

--- udemy_P_course/training.py
+++ udemy_P_course/training.py
@@ -6,10 +6,34 @@
 #    \ \____________\ \_______\ \__\\ \__\ \_______\
 #     \|____________|\|_______|\|__| \|__|\|_______|
 
-def f1():
-    x = 100
-    print(x)
+#Write your code below this line 👇
+# import  math
 
-x =+ 1
-# print(x)
-# f1()       
+# def paint_calc(height , width , cover):
+#     result = (height * width) / cover
+#     print(f"You'll need {math.ceil(result)} cans of paint.")
+
+
+
+#Write your code above this line 👆
+# Define a function called paint_calc() so that the code below works.   
+
+# 🚨 Don't change the code below 👇
+# test_h = int(input("Height of wall: "))
+# test_w = int(input("Width of wall: "))
+# coverage = 5
+# paint_calc(height=test_h, width=test_w, cover=coverage)
+
+# def primeNumber(number):
+#     tabPrime = []
+#     for i in range(1 , number + 1):
+#         if number % i == 0:
+#             tabPrime.append(i)
+#     if len(tabPrime) == 2:
+#         print("It's a prime number")
+#     else:
+#         print("Its not a prime number")    
+
+# n = int(input("Check this number: "))          
+# primeNumber(n)        
+
