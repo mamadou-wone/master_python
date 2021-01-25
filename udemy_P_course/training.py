d2d940654006bb5ccf225a93966a20696c976ab5
Mamadou WONE <mamadouwone12345@gmail.com>
2021-01-25 11:03:11 +0000

    Start day 10

--- udemy_P_course/training.py
+++ udemy_P_course/training.py
@@ -6,95 +6,63 @@
 #    \ \____________\ \_______\ \__\\ \__\ \_______\
 #     \|____________|\|_______|\|__| \|__|\|_______|
 
-# student_scores = {
-#   "Harry": 81,
-#   "Ron": 78,
-#   "Hermione": 99, 
-#   "Draco": 74,
-#   "Neville": 62,
-# }
-# # 🚨 Don't change the code above 👆
-
-# #TODO-1: Create an empty dictionary called student_grades.
-# student_grades = {}
-
-# #TODO-2: Write your code below to add the grades to student_grades.👇
-# for key in student_scores:
-#     if student_scores[key] <= 70:
-#         student_grades[key] = "Fail"
-#     elif student_scores[key] > 70 and student_scores[key] <= 80:
-#         student_grades[key] = "Acceptable"
-#     elif student_scores[key] > 80 and student_scores[key] <=90:
-#         student_grades[key] = "Exceeds Expectations"
-#     elif student_scores[key] > 900 and student_scores[key] <=100:
-#          student_grades[key] = "Outstanding"
-# # 🚨 Don't change the code below 👇
-# print(student_grades)
-
-# travel_log = [
-# {
-#   "country": "France",
-#   "visits": 12,
-#   "cities": ["Paris", "Lille", "Dijon"]
-# },
-# {
-#   "country": "Germany",
-#   "visits": 5,
-#   "cities": ["Berlin", "Hamburg", "Stuttgart"]
-# },
-# ]
-# #🚨 Do NOT change the code above
-
-# #TODO: Write the function that will allow new countries
-# #to be added to the travel_log. 👇
-# def add_new_country(country , visits, cities = []):
-#     travel_log.append(
-#        { "country": country,
-#         "visits": visits,
-#         "cities": cities}
-#     )
-
-
-
-
-# #🚨 Do not change the code below
-# add_new_country("Russia", 2, ["Moscow", "Saint Petersburg"])
-
-# print(travel_log)
-
-from art_hagman import massu
-
-print(massu)
-
-enchere = []
-def add_bidders(name, bid):
-    enchere.append(
-        {
-            "name": name,
-            "bid": bid
-        }
-    )
-
-continu = ''
+# def format_name(firstName, lastName):
+#     first = ''
+#     name = ''
+#     completeName  = ''
+#     for i in range(1 , len(firstName)):
+#         first += firstName[i].lower()
+#     first = firstName[0].upper()+first
     
-while continu != 'no':
-    name = input("What is your name ? ")
-    bid = int(input("What is your bid ? $"))
-    confirm = input("Are there any other bidders? Type 'yes' or 'no'\n")
-    continu = confirm
-    add_bidders(name , bid)
-
-# print(enchere)
-maximun = 0
-winner = ''
-for key in enchere:
-    if key['bid'] > maximun:
-        maximun = key['bid']
-        winner = key['name']
+#     for i in range(1 ,len(lastName)):
+#         name += lastName[i].lower()
+#     name = lastName[0].upper()+name
+#     completeName = first + ' ' + name
         
-print(f"The winner is {winner} with a bid of ${maximun}")    
-    
-
+#     return completeName    
     
     
 
+# test=  format_name("MAMAdou", "wONE")
+# print(test)
+
+# def is_leap(year):
+#   leap =  False
+#   if year % 4 == 0:
+#     if year % 100 == 0:
+#       if year % 400 == 0:
+#         leap = True
+#       else:
+#         leap = False
+#     else:
+#      leap = True
+#   else:
+#     leap = False
+#   return leap  
+
+# def days_in_month(year , month):
+#   month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31] 
+#   days = 0 
+#   if is_leap(year) and month == 2:
+#     #   month_days[1] = 29
+#     #   month_days = month_days[month]
+#     days = 29
+#   else:
+#       days = month_days[month - 1]
+#   return days        
+  
+# #🚨 Do NOT change any of the code below 
+# year = int(input("Enter a year: "))
+# month = int(input("Enter a month: "))
+# days = days_in_month(year, month)
+# print(days)
+
+def my_function(a):
+    if a < 40:
+        return
+        print("Terrible")
+    if a < 80:
+        return "Pass"
+    else:
+        return "Great"
+print(my_function(25))
