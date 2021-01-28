381486fef6ecfecd9b4333c73f4247ace90c15fc
Mamadou WONE <mamadouwone12345@gmail.com>
2021-01-28 12:45:03 +0000

    Day14

--- udemy_P_course/training.py
+++ udemy_P_course/training.py
@@ -6,33 +6,56 @@
 #    \ \____________\ \_______\ \__\\ \__\ \_______\
 #     \|____________|\|_______|\|__| \|__|\|_______|
 
+from art_hagman import  guess
+from art_hagman import vs
+from data import data
 import random
 
-print("Welcome to the Guessing Game !")
-print("I'm thinking of a number between 11 and 100")
-choose = input("Choose a difficulty . Type 'easy' or 'hard': ")
+print(guess)
 
-attempts = 0
 
-if choose == "easy":
-    attempts = 10
-else:
-    attempts = 5
-        
-randomNumber = random.randint(1, 100)
-print(randomNumber)
-def guess(randomNumber , attempts):
-    print(f"You have {attempts} to guess the number")
-    while attempts != 0:
-        user_number = int(input("Make a guess : "))
-        if user_number < randomNumber:
-            print("Too Low")
-        elif user_number > randomNumber:
-            print("Too high")
-        else:
-            print(f"You guess the number {randomNumber}")
-            break        
-        attempts -= 1    
-        print(f"You have {attempts} to guess the number")
+# firstData = random.choice(data)
+# secondData = random.choice(data)
 
-guess(randomNumber , attempts)
+def chooseData(data = []):
+  randomData = random.choice(data)
+  return randomData
+    
+  
+firstData = chooseData(data)
+secondData = chooseData(data)
+
+def compare(firstData={} , secondData = {}):
+  if firstData == secondData:
+    secondData = chooseData(data)
+  else:
+    print("Compare A : " + firstData['name'] + ", a "  + firstData['description'] + " , "+ " from " + firstData['country'])        
+    print(vs)
+    print("Against B : " + secondData['name'] + ", a "  + secondData['description'] + " , "+ " from " + secondData['country']) 
+
+
+win = False
+
+
+def checkAnsuwer(answer , firstData = {} , secondData = {} , data = []):
+    if answer == "A" and firstData['follower_count'] > secondData['follower_count']:
+        print('You win continu ! ')
+        win = True
+        secondData = chooseData(data)
+        compare(firstData, secondData)
+    elif answer == "B" and firstData['follower_count'] < secondData['follower_count']:
+        thirdData = chooseData(data)
+        firstData = secondData
+        secondData = thirdData
+        print('You Win continu ! ')
+        win = True
+        compare(firstData, secondData)  
+    else:
+      print("You Loose ")        
+      win = False
+    
+compare(firstData , secondData)     
+answer = input("Who has more followers? Type 'A' or 'B' : ")
+
+checkAnsuwer(answer , firstData , secondData , data)    
+    
