7abc88a50d4c9211c8306068e90f82e2d577de6b
Mamadou WONE <mamadouwone12345@gmail.com>
2021-01-31 17:29:21 +0000

    Finis Day15

--- udemy_P_course/training.py
+++ udemy_P_course/training.py
@@ -6,64 +6,35 @@
 #    \ \____________\ \_______\ \__\\ \__\ \_______\
 #     \|____________|\|_______|\|__| \|__|\|_______|
 
-from art_hagman import  guess
-from art_hagman import vs
-from data import data
-import random
+MENU = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "coffee": 18,
+        },
+        "cost": 1.5,
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24,
+        },
+        "cost": 2.5,
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 250,
+            "milk": 100,
+            "coffee": 24,
+        },
+        "cost": 3.0,
+    }
+}
 
-# print(guess)
+resources = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+}
 
-
-# firstData = random.choice(data)
-# secondData = random.choice(data)
-
-def chooseData(data = []):
-  randomData = random.choice(data)
-  return randomData
-    
-  
-firstData = chooseData(data)
-secondData = chooseData(data)
-
-
-def compare(firstData = {} , secondData = {},data = []):
-    if firstData == secondData:
-        secondData = chooseData(data)         
-    else:
-          print(f"Compare A: {firstData['name']} , a {firstData['description']}, from {firstData['country']} ")
-          print(vs)
-          print(f"Compare B: {secondData['name']} , a {secondData['description']}, from {secondData['country']} ")
-
-
-
-win = False
-score = 0
-compare(firstData , secondData , data) 
-while win != True: 
-  answer = input("Who has more followers ? Type A or B: ")
-  if answer == "A" and firstData['follower_count'] > secondData['follower_count']:
-        print("You Win")
-        secondData = chooseData(data)
-        win = True
-        score += 1
-        compare(firstData, secondData, data)
-        win = False
-  elif answer == "B" and firstData['follower_count'] < secondData['follower_count']:
-        firstData = secondData
-        secondData = chooseData(data)
-        win = True
-        score += 1
-        compare(firstData, secondData, data)
-        print("You Win")
-        win = False
-  else:
-      print('You Loose')
-      win = False
-      break 
-              
-
-print(f"Your score is {score}")      
-
-
-# print(firstData )
-# print(secondData)
