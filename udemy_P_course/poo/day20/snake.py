b3b40636de862b7037ec5c8eaa7769a07cb5b6a2
Mamadou WONE <mamadouwone12345@gmail.com>
2021-02-06 19:01:57 +0000

    Day20 part 1

---
+++ udemy_P_course/poo/day20/snake.py
@@ -0,0 +1,46 @@
+from turtle import Turtle
+MOUVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+class Snake:
+    def __init__(self):
+        self.starting_position = [(0 , 0), (-20, 0), (-40 , 0)]
+        self.all_snake = []
+        self.create_snake()
+
+    def create_snake(self):
+        for position in self.starting_position:
+            snake = Turtle("square")
+            snake.color("white")
+            snake.penup()
+            snake.goto(position)
+            self.all_snake.append(snake)
+        
+
+        
+    def move(self):
+        for snake_num in range(len(self.all_snake) - 1 , 0 , -1):
+            new_x = self.all_snake[snake_num - 1].xcor()
+            new_y = self.all_snake[snake_num - 1].ycor()
+            self.all_snake[snake_num].goto(new_x , new_y)
+
+        self.all_snake[0].forward(MOUVE_DISTANCE)
+
+
+    def up(self):
+        if self.all_snake[0].heading() != DOWN:
+            self.all_snake[0].setheading(UP)
+        
+    def down(self):
+        if self.all_snake[0].heading() != UP:
+            self.all_snake[0].setheading(DOWN)
+        
+    def left(self):
+        if self.all_snake[0].heading() != RIGHT:
+            self.all_snake[0].setheading(LEFT)
+        
+    def right(self):
+        if self.all_snake[0].heading() != LEFT:
+            self.all_snake[0].setheading(RIGHT)
