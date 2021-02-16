11bcd1498d8660f9d747112105668834734f46bf
Mamadou WONE <mamadouwone12345@gmail.com>
2021-02-16 19:28:51 +0000

    day 28

---
+++ udemy_P_course/poo/day28/pomodoro-start/main.py
@@ -0,0 +1,80 @@
+from tkinter import *
+import math
+# ---------------------------- CONSTANTS ------------------------------- #
+PINK = "#e2979c"
+RED = "#e7305b"
+GREEN = "#9bdeac"
+YELLOW = "#f7f5dd"
+FONT_NAME = "Courier"
+WORK_MIN = 25
+SHORT_BREAK_MIN = 5
+LONG_BREAK_MIN = 20
+reps = 0
+# ---------------------------- TIMER RESET ------------------------------- # 
+
+# ---------------------------- TIMER MECHANISM ------------------------------- # 
+def start_timer():
+    global reps
+    reps += 1
+    work_sec = 1 * 60
+    short_work_sec = SHORT_BREAK_MIN * 60
+    lon_break_sec = LONG_BREAK_MIN * 60
+    if reps % 8 == 0:
+        start_timer(lon_break_sec)
+        timer.config(text="Break", fg=RED)
+    elif reps % 2 == 0:
+        count_down(short_work_sec)
+        timer.config(text="Break", fg=RED)
+    else:
+        count_down(work_sec)
+        timer.config(text="Work", fg=GREEN)
+
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+def count_down(count):
+    count_min = math.floor(count / 60)
+    count_sec = count % 60
+    if count_sec == 0:
+        count_sec = "00"
+    elif count_sec < 10:
+        count_sec = f"0{count_sec}"
+
+    canvas.itemconfig(canvas_text, text=f"{count_min}:{count_sec}")
+    if count > 0:
+        window.after(1000, count_down, count-1)
+    else:
+        start_timer()
+
+# ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Pomodoro")
+window.config(padx=100, pady=50, bg=YELLOW)
+
+
+
+# Timer
+timer = Label(text="Timer", font=(FONT_NAME, 35, "bold"), fg=GREEN, bg=YELLOW)
+timer.grid(column=1, row=0)
+
+#Start Button
+start_button = Button(text="Start", highlightthickness=0, command=start_timer)
+start_button.grid(column=0, row=2)
+
+# Reset Button
+reset_button = Button(text="Reset", highlightthickness=0)
+reset_button.grid(column=2, row=2)
+
+# Check Marker
+check_marker = Label(text="✔", font=(FONT_NAME, 10, "bold"), fg=GREEN, bg=YELLOW)
+check_marker.grid(column=1, row=3)
+
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
+image = PhotoImage(file="tomato.png")
+canvas.create_image(100, 112, image=image)
+canvas_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+canvas.grid(column=1, row=1)
+
+
+
+
+window.mainloop()
