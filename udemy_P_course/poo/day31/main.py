219f14f033d3c031d60c7b5eb2b6a3faab0eb361
Mamadou WONE <mamadouwone12345@gmail.com>
2021-02-21 10:28:44 +0000

    finish day31

--- udemy_P_course/poo/day31/main.py
+++ udemy_P_course/poo/day31/main.py
@@ -5,21 +5,42 @@ import random
 time = None
 BACKGROUND_COLOR = "#B1DDC6"
 
-data = pandas.read_csv("data/french_words.csv")
+data = None
+
+try:
+    data = pandas.read_csv("data/word_to_learn.csv")
+except FileNotFoundError:
+    data = pandas.read_csv("data/french_words.csv")
+
 
 new_dict = data.to_dict(orient="records")
 current_card = {}
 
+word_to_learn = []
 
 def new_word():
-    global current_card
+    global current_card, flip_timer
+    window.after_cancel(flip_timer)
     current_card = random.choice(new_dict)
     canvas.itemconfig(title, text="French")
     canvas.itemconfig(word, text=current_card["French"])
     canvas.itemconfig(card_bacground, image=card_front)
     canvas.itemconfig(title, fill="black")
     canvas.itemconfig(word, fill="black")
-    # start_count(3)
+    flip_timer = window.after(3000, func=flip_card)
+
+
+def next():
+    global word_to_learn, new_dict
+    new_dict.remove(current_card)
+    # print(len(new_dict))
+    word_to_learn = new_dict
+    new_csv = pandas.DataFrame(word_to_learn)
+    new_csv.to_csv("data/word_to_learn.csv", index=False)
+    new_word()
+
+
+
 
 def flip_card():
     canvas.itemconfig(title, text="English")
@@ -28,8 +49,10 @@ def flip_card():
     canvas.itemconfig(title, fill="white")
     canvas.itemconfig(word, fill="white")
 
+
+
 window = Tk()
-window.after(3000, func=flip_card)
+flip_timer = window.after(3000, func=flip_card)
 
 window.title("Boss")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
@@ -50,7 +73,7 @@ cancel_image = PhotoImage(file="images/wrong.png")
 yes_image = PhotoImage(file="images/right.png")
 
 # Button
-rigth_butoon = Button(image=yes_image, highlightthickness=0, command=new_word)
+rigth_butoon = Button(image=yes_image, highlightthickness=0, command=next)
 rigth_butoon.grid(column=1, row=1)
 wrong_button = Button(image=cancel_image, highlightthickness=0, command=new_word)
 wrong_button.grid(column=0, row=1)
