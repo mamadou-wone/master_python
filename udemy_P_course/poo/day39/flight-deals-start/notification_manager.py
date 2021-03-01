d2c5834b9490eb71b02378c3a331387ed9e123b8
Mamadou WONE <mamadouwone12345@gmail.com>
2021-03-01 20:40:56 +0000

    Start day40

--- udemy_P_course/poo/day39/flight-deals-start/notification_manager.py
+++ udemy_P_course/poo/day39/flight-deals-start/notification_manager.py
@@ -1,7 +1,7 @@
 import requests
 from twilio.rest import Client
-ACCOUNT_SID = "AC2f66923cc89ca72ea11f17a038d02c85"
-AUTH_TOKEN = "bf61f67c06df94e909ff2ed9b4801f64"
+ACCOUNT_SID = ""
+AUTH_TOKEN = ""
 class NotificationManager:
     #This class is responsible for sending notifications with the deal flight details.
     def __init__(self):
