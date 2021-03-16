3851b19ade5131916de83c04ded3fa3ea39eed04
Mamadou WONE <mamadouwone12345@gmail.com>
2021-03-16 11:55:56 +0000

    finish day 59 & 60 blop upgrade

--- udemy_P_course/poo/day59/upgraded-blog/main.py
+++ udemy_P_course/poo/day59/upgraded-blog/main.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from post import Post
 import requests
+from smtplib import SMTP
 
 API_ENDPOINT = 'https://api.npoint.io/43644ec4f0013682fc0d'
 
@@ -15,6 +16,14 @@ def fetch_post(url):
         post_list.append(post)
     return post_list
 
+def send_email(name, email, phone, message):
+    with SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user="megawone735@gmail.com", password="775596731")
+        connection.sendmail(from_addr="megawone735@gmail.com", to_addrs="mamadouwone12345@gmail.com",
+                            msg="Subject:Want to Contact You\n\n"
+                                f"Name: {name}\n Email: {email} \n Phone: {phone}\n Message: {message}"
+                            )
 
 @app.route('/')
 def home():
@@ -28,7 +37,8 @@ def about():
 
 @app.route('/contact')
 def contact():
-    return render_template('contact.html')
+    msg_send = "Contact me!"
+    return render_template('contact.html', msg_send=msg_send)
 
 @app.route('/post/<post_id>')
 def post(post_id):
@@ -36,6 +46,16 @@ def post(post_id):
     return render_template('post.html', post_id=int(post_id), all_post=post_list)
 
 
+@app.route('/form-entry', methods=['POST', 'GET'])
+def receive_data():
+    error = None
+    msg_send = None
+    if request.method == 'POST':
+        send_email(request.form["name"], request.form["email"], request.form["phone"], request.form["message"])
+        msg_send = "Message Send SuccessFull"
+        return render_template('contact.html', msg_send=msg_send)
+    msg_send = "Error"
+    return render_template('contact.html', msg_send=msg_send)
 
 if __name__ == '__main__':
     app.run(debug=True)
