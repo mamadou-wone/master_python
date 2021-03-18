c51795f86d1b6ab52f0b8957e3f4316f305b633d
Mamadou WONE <mamadouwone12345@gmail.com>
2021-03-18 17:04:43 +0000

    day63

---
+++ udemy_P_course/poo/day63/library-start/main.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+all_books = []
+
+
+@app.route('/')
+def home():
+    global all_books
+    return render_template('index.html', books=all_books)
+
+
+@app.route("/add", methods=['POST', 'GET'])
+def add():
+    global all_books
+    if request.method == "POST":
+        new_book = {
+            "title": request.form['title'],
+            "author": request.form['author'],
+            "rating":  request.form['rating']
+        }
+        all_books.append(new_book)
+        print(all_books)
+    return render_template('add.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
