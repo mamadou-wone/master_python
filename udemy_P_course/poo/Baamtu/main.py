b0ce1eb4894d3f0ee569dc9cd8a755f43a0e4367
mwone472 <mamadouwone12345@gmail.com>
2021-04-18 20:35:42 +0000

    18/04/2021

--- udemy_P_course/poo/Baamtu/main.py
+++ udemy_P_course/poo/Baamtu/main.py
@@ -6,11 +6,21 @@
 #    \ \____________\ \_______\ \__\\ \__\ \_______\
 #     \|____________|\|_______|\|__| \|__|\|_______|
 
+for i in range(10, 0, -1):
+    print(f"You are a Boss {i}")
 
-text = "Welcome Boss"
 
-try:
-    with open("test.txt") as file:
-        pass
-except FileNotFoundError:
-    new_file = open("text.txt", "w")
+
+
+
+
+
+
+#
+# text = "Welcome Boss"
+#
+# try:
+#     with open("test.txt") as file:
+#         pass
+# except FileNotFoundError:
+#     new_file = open("text.txt", "w")
