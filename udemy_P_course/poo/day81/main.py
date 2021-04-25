0794baa3dd207c45bd8989ed3940e57caa8d875b
mwone472 <mamadouwone12345@gmail.com>
2021-04-25 15:54:57 +0000

    scrapping for morse code generato

--- udemy_P_course/poo/day81/main.py
+++ udemy_P_course/poo/day81/main.py
@@ -1,5 +1,54 @@
 # from utils import morse_code
+from pprint import pprint
+
 import pandas
+
+data = pandas.read_csv('code.csv')
+
+letters_dict = data.to_dict()
+code_list = {}
+
+for i in range(len(letters_dict['Key'])):
+    code_list[letters_dict['Key'][i]] = letters_dict['Value'][i]
+
+info = input("Enter your name: ").upper()
+
+morse_code = ""
+for let in info:
+    if code_list[let]:
+        morse_code += code_list[let]
+    else:
+        print("error")
+
+print(morse_code)
+
+# for val in data.iterrows():
+#     print(val[1]['Key']['Value'])
+
+# for key in data['Key']:
+#     print(key[0])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # letters = [key for key in morse_code]
 # code = [morse_code[key] for key in morse_code]
@@ -14,10 +63,10 @@ import pandas
 # print(test[::3])
 # [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 # [0, 3, 6, 9]
-test = ['A', '?', 'N', '?', 'B', '?', 'O', '?', 'C', '?', 'P', '?', 'D', '?', 'Q', '?', 'E', '?', 'R', '?', 'F', '?', 'S', '?', 'G', '?', 'T', '?', 'H', '?', 'U', '?', 'I', '?', 'V', '?', 'J', '?', 'W', '?', 'K', '?', 'X', '?', 'L', '?', 'Y', '?', 'M', '?', 'Z', '?']
-# ['A', '?', 'N']
-
-print(test[::4])
+# test = ['A', '?', 'N', '?', 'B', '?', 'O', '?', 'C', '?', 'P', '?', 'D', '?', 'Q', '?', 'E', '?', 'R', '?', 'F', '?', 'S', '?', 'G', '?', 'T', '?', 'H', '?', 'U', '?', 'I', '?', 'V', '?', 'J', '?', 'W', '?', 'K', '?', 'X', '?', 'L', '?', 'Y', '?', 'M', '?', 'Z', '?']
+# # ['A', '?', 'N']
+#
+# print(test[::4])
 
 # import csv
 #
--- udemy_P_course/poo/day81/utils.py
+++ udemy_P_course/poo/day81/utils.py
@@ -38,9 +38,10 @@ morse_code = {
 # print(code)
 from selenium import webdriver
 import time
+import pandas
 
-URL = "https://fr.wikipedia.org/wiki/Code_Morse_international"
-
+# URL = "https://fr.wikipedia.org/wiki/Code_Morse_international"
+URL = 'https://www.electronics-notes.com/articles/ham_radio/morse_code/characters-table-chart.php'
 path = "/home/mamadou/BOSS/webdriver/chromedriver"
 
 driver = webdriver.Chrome(executable_path=path)
@@ -49,13 +50,41 @@ driver.get(URL)
 
 
 time.sleep(5)
-content = driver.find_elements_by_xpath('//*[@id="mw-content-text"]/div[1]/table[1]/tbody')
-# letters = [item for item in content]
-
-letters = []
+content = driver.find_elements_by_xpath('//*[@id="primary"]/article/table/tbody')
+table_list = []
 for item in content:
-    letters = item.find_elements_by_tag_name('a')
+    table_list = item.find_elements_by_tag_name('td')
 
-letter = [let.text for let in letters if len(let.text) == 1]
-print(letter)
-print(letter[:3])
+count = 2
+letters_dict = {}
+key = ""
+value = ""
+for td in range(len(table_list) - 6):
+    try:
+        key = table_list[td].find_element_by_xpath('//*[@id="primary"]/article/table/tbody/tr['+f"{count}"+']/td[2]').text
+        value = table_list[td].find_element_by_xpath(
+            '//*[@id="primary"]/article/table/tbody/tr[' + f"{count}" + ']/td[3]').text
+        letters_dict[key] = value
+
+        key = table_list[td].find_element_by_xpath(
+            '//*[@id="primary"]/article/table/tbody/tr[' + f"{count}" + ']/td[5]').text
+        value = table_list[td].find_element_by_xpath(
+            '//*[@id="primary"]/article/table/tbody/tr[' + f"{count}" + ']/td[6]').text
+        letters_dict[key] = value
+        count += 1
+    except:
+        count += 1
+
+
+
+# print(letters_dict)
+
+elements = [key for key in letters_dict]
+code = [letters_dict[key] for key in letters_dict]
+
+myFile = pandas.Series(code, elements)
+myFile.to_csv("code.csv")
+
+
+
+
